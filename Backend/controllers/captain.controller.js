796e6e2fe43c00944c514568d9714086f05e82d2
Prathmesh Jain <prathmeshjain046@gmail.com>
2025-09-26 02:36:13 +0530

    captian authentication  done

--- Backend/controllers/captain.controller.js
+++ Backend/controllers/captain.controller.js
@@ -1,6 +1,7 @@
 const captainModel = require("../models/captain.model");
 const captainService = require("../services/captain.service");
 const { validationResult } = require("express-validator");
+const blackListTokenModel = require("../models/blackListToken.model");
 
 module.exports.registerCaptain = async (req, res, next) => {
 
@@ -35,4 +36,48 @@ module.exports.registerCaptain = async (req, res, next) => {
 
     res.status(201).json({token, captain});
         
+}
+
+module.exports.loginCaptain = async (req, res, next) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    
+    const captain = await captainModel.findOne({ email }).select("+password");
+
+    if(!captain) {
+        return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const isMatch = await captain.comparePassword(password);
+    if(!isMatch) {
+        return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const token = captain.generateAuthToken();
+    
+    res.cookie("token", token);
+    res.status(200).json({ token, captain });
+
+}
+
+module.exports.getCaptainProfile = async (req, res, next) => {
+    res.status(200).json({ captain: req.captain });
+}
+
+module.exports.logoutCaptain = async (req, res, next) => {
+    const token = req.cookies.token || req.header('Authorization')?.replace('Bearer ', '')[1];
+    if(!token) {
+        return res.status(400).json({ message: "No token provided" });
+    }
+
+    const blackListToken = new blackListTokenModel({ token });
+    await blackListToken.save();
+
+    res.clearCookie("token");
+    res.status(200).json({ message: "Logged out successfully" });
 }
--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,6 +1,8 @@
 const userModel = require("../models/user.model");
+const captainModel = require("../models/captain.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const blackListTokenModel = require("../models/blackListToken.model");
 
 module.exports.authUser = async (req, res, next) => {
    const token = req.cookies.token || req.header('Authorization')?.replace('Bearer ', '');
@@ -8,7 +10,7 @@ module.exports.authUser = async (req, res, next) => {
         return res.status(401).json({ message: "Access denied. No token provided." });
     }
 
-    const isBlacklisted = await userModel.findOne({token : token});
+    const isBlacklisted = await blackListTokenModel.findOne({token : token});
     if(isBlacklisted) {
         return res.status(401).json({ message: "Unauthorised" });
     }
@@ -17,6 +19,28 @@ module.exports.authUser = async (req, res, next) => {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const user = await userModel.findById(decoded._id);
         req.user = user;
+
+        next();
+    } catch (err) {
+        res.status(400).json({ message: "Invalid token." });
+    }
+}
+
+module.exports.authCaptain = async (req, res, next) => {
+   const token = req.cookies.token || req.header('Authorization')?.replace('Bearer ', '');
+    if(!token) {
+        return res.status(401).json({ message: "Access denied. No token provided." });
+    }
+
+    const isBlacklisted = await blackListTokenModel.findOne({token : token});
+    if(isBlacklisted) {
+        return res.status(401).json({ message: "Unauthorised" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const captain = await captainModel.findById(decoded._id);
+        req.captain = captain;
 
         next();
     } catch (err) {
